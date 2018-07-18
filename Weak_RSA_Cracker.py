e3dc4691d2881a8e19378d070a103cdeae1ab6e2
JYCsec <40046083+JYCSEC@users.noreply.github.com>
2018-07-18 11:32:01 -0400

    Decrypts with n,e and euler's totient of n by generating private key

--- Weak_RSA_Cracker.py
+++ Weak_RSA_Cracker.py
@@ -8,10 +8,11 @@ key = RSA.importKey(f.read())
 print ("n is = " + str(key.n))
 print ("e is = " + str(key.e))
 
-n=key.n
 t = 0
-for i in range(1,n+1):
-    if fractions.gcd(n,i)==1:
+for i in range(1,key.n+1):
+    if fractions.gcd(key.n,i)==1:
         t += 1
 print ("Euler Totient is " + str(t))
-
+priv_key = RSA.construct(key.n,key.e,t)
+out = priv_key.decrypt(open(raw_input("Enter encrypted file: "),'r'))
+print ("Decrypted message is" + out)
