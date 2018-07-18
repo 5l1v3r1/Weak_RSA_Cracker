e966290aa5f4e4690aff4879e3d3c6f987dc1679
root <root@localhost.localdomain>
2018-07-18 10:17:11 -0400

    Easier read and hardcoded for testing

--- Weak_RSA_Cracker.py
+++ Weak_RSA_Cracker.py
@@ -1,6 +1,8 @@
+
 from Crypto.PublicKey import RSA
 
-f = open(raw_input("Enter pub key: "),'r')
+#f = open(raw_input("Enter pub key: "),'r')
+f = open ('key.pub','r')
 key = RSA.importKey(f.read())
-print key.n
-print key.e
+print "n is = " + str(key.n)
+print "e is = " + str(key.e)
