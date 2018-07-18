a80091d39c08388971d5c2ee05744f5193263ff5
JYCSEC <40046083+JYCSEC@users.noreply.github.com>
2018-07-18 10:49:15 -0400

    Changed to Python3.6 to get pass Overflow error due to limitation of Python2.7

--- Weak_RSA_Cracker.py
+++ Weak_RSA_Cracker.py
@@ -1,8 +1,17 @@
 #JYCSEC
 from Crypto.PublicKey import RSA
+import fractions
 
 #f = open(raw_input("Enter pub key: "),'r')
 f = open ('key.pub','r')
 key = RSA.importKey(f.read())
-print "n is = " + str(key.n)
-print "e is = " + str(key.e)
+print ("n is = " + str(key.n))
+print ("e is = " + str(key.e))
+
+n=key.n
+t = 0
+for i in range(1,n+1):
+    if fractions.gcd(n,i)==1:
+        t += 1
+print ("Euler Totient is " + str(t))
+
